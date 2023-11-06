0b58522077ead23a7be5e480f1cac147e751324f
Marcele <marcelecardozo@gmail.com>
2023-11-06 20:43:09 -0300

    inserting render link

--- src/config/services/api.service.ts
+++ src/config/services/api.service.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://atividade-final-backend-2.onrender.com",
 });
 
 console.log(apiService.head);
